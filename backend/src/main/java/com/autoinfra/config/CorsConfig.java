88c3e6d25bc64e4b64a72ad7d8b1d1f1513f5054
amrutha-learn <ammuamrutha8400@gmail.com>
2026-02-23 22:43:35 +0530

    Stable working version - frontend + backend connected

--- backend/src/main/java/com/autoinfra/config/CorsConfig.java
+++ backend/src/main/java/com/autoinfra/config/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                       .allowedOrigins(
+    "https://error-analyzer-infra.onrender.com",
+    "https://error-analyzer-infra.vercel.app",
+    "https://error-analyzer-infra-cxnfzbjvg-amrutha-mjs-projects.vercel.app"
+)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
